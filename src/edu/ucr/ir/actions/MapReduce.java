a637149335d6fe99c90a6db51bdd073912ebff26
William Mac <wmac001@ucr.edu>
2020-03-09 18:46:07 -0700

    Updated the iteration.

--- src/edu/ucr/ir/actions/MapReduce.java
+++ src/edu/ucr/ir/actions/MapReduce.java
@@ -206,8 +206,8 @@ public class MapReduce {
 
 		private static final String fileTag = "INDEX~";
 		private static final String MR_DATA_SEPARATOR = "\t";
-		private static final String WEBPAGE_DS = "\"url\":\"";
-		private static final String BODY_DS = "\"body\":\"";
+		private static final String WEBPAGE_DS = "url";
+		private static final String BODY_DS = "body";
 		private static final String WORD_DS = " ";
 
 		public void map(Text key, Text value, Context context
