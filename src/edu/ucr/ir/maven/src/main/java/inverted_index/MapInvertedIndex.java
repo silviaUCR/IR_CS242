817dd156ff9079234655558716e129fefd7ce0e1
William Mac <wmac001@ucr.edu>
2020-03-13 21:16:46 -0700

    Inverted Index works! Need to fine tune stop word techniques.

--- src/edu/ucr/ir/maven/src/main/java/inverted_index/MapInvertedIndex.java
+++ src/edu/ucr/ir/maven/src/main/java/inverted_index/MapInvertedIndex.java
@@ -1,35 +1,79 @@
 package inverted_index;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MapInvertedIndex
         extends Mapper<Text, Text, Text, Text> {
-    private Text word = new Text();
-    private Text url_tf = new Text();
-    private static final String MR_DATA_SEPARATOR = "\t";
-    public static enum CustomCounter {
-        RES_CNT,
+    private Text word_url_key = new Text();
+    private Text value = new Text();
 
+    public static enum CustomCounter {
+        WORD_CNT,
+        DOCUMENT_CNT,
     }
+
+    private static final String fileTag = "INDEX~";
+    private static final String MR_DATA_SEPARATOR = "\t";
+    private static final String WORD_DS = " ";
+    ArrayList<String> allWords;
+
+    static List<String> stopwords;
+
     public void map(Text key, Text value, Context context
     ) throws IOException, InterruptedException {
 
-        if(isNullOrEmpty(value.toString())) {
+        String jsonLine = value.toString();
+        JsonElement jelement = new JsonParser().parse(jsonLine);
+        JsonObject jobject = jelement.getAsJsonObject();
+        stopwords = Files.readAllLines(Paths.get("stopword/StopWordList.txt"));
 
-        } else {
-            String word_url = key.toString();
-            String[] tokens = word_url.split(MR_DATA_SEPARATOR);
-            //String url = tokens[1];
-            String tf = value.toString();
-            word.set(tokens[0]);
-            //url_tf.set (url + MR_DATA_SEPARATOR + tf);
-            url_tf.set (tf);
-            context.write(word,url_tf);
+
+        //ArrayList<String> urls = new ArrayList<String>(); TO STORE STRINGS - KEEP FOR LATER IF NEEDED
+        JsonArray jarray = jobject.getAsJsonArray("pages");
+        for (JsonElement urlHolder : jarray) {
+            //urls.add(urlHolder.getAsJsonObject().getAsJsonPrimitive("url").getAsString()); // KEEP FOR LATER IF NEEDED
+            String url = urlHolder.getAsJsonObject().getAsJsonPrimitive("url").getAsString();
+            String body = urlHolder.getAsJsonObject().getAsJsonPrimitive("body").getAsString();
+            //body = body.replaceAll("[^\\p{ASCII}]", "");
+            body = body.replaceAll("[^a-zA-Z0-9_-_ ]", "");
+
+            //allWords.clear();
+
+            allWords =
+                    Stream.of(body.toLowerCase().split(" "))
+                            .collect(Collectors.toCollection(ArrayList<String>::new));
+            allWords.removeAll(stopwords);
+
+
+            for (String word : allWords) {
+                /*
+                final Pattern p = Pattern.compile("\\W");
+                final Matcher matcher = p.matcher(word);
+                matcher.find();
+                word = matcher.group(1);*/
+                if(isNullOrEmpty(word)) {
+
+                } else {
+                    word_url_key.set(word);  //creates the key
+                    value.set(url);  //creates the value. 1 is just a dummy variable
+                    context.write(word_url_key, value);
+                }
+
+            }
         }
-
     }
 
     public static boolean isNullOrEmpty(String str) {
@@ -37,4 +81,6 @@ public class MapInvertedIndex
             return false;
         return true;
     }
+
+
 }
